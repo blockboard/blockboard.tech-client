6575d29b5d3290d9157fcc40f821cab8c89ed1c7
AhmedSoror <ahmedsoror438@gmail.com>
2021-02-28 18:20:26 +0200

    /src/components/theme/Header/NavbarLinks/index.jsx: - route to blog page

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -3,7 +3,7 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { ThemeContext } from 'providers/ThemeProvider';
 import ToggleTheme from 'components/theme/Header/ToggleTheme';
 import { Wrapper } from './styles';
-
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 const NavbarLinks = ({ desktop }) => {
   const { theme } = useContext(ThemeContext);
 
@@ -12,6 +12,9 @@ const NavbarLinks = ({ desktop }) => {
       <AnchorLink href="#about">About</AnchorLink>
       <AnchorLink href="#projects">Projects</AnchorLink>
       <AnchorLink href="#contact">Contact</AnchorLink>
+      <Router>
+          <Link to="/blog" target="_blank">blog</Link>        
+      </Router>
       <ToggleTheme />
     </Wrapper>
   );
